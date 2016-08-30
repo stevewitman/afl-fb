b80cdde82f4928c2a375c2e69aeff06a51bf2f42
Steve Witman <stevewitman@gmail.com>
2016-08-29 23:53:05 -0600

    started basic site and enabled google auth

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('here');
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+function signIn() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.displayName);
+    console.log(user.email);
+    console.log(user.photoURL);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
